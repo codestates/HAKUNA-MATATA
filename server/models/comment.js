9c5924e897ecbd46193590ee50f22e565b479340
hanamon <devparkhana@gmail.com>
2021-10-07 17:30:33 +0900

    Add sequelize models

---
+++ server/models/comment.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Comment.init(
+    {
+      comment_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER(11)
+      },
+      user_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER(11)
+      },
+      post_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER(11)
+      },
+      comment_content: {
+        allowNull: false,
+        type: DataTypes.STRING(255)
+      },
+      created_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: new Date()
+      },
+      updated_at: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: new Date()
+      }
+    },
+    {
+      sequelize,
+      modelName: 'comment',
+      id: 'comment_id',
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  );
+  return Comment;
+};
