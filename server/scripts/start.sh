2a2757f06db2dd47904a0ec0247434180214d5ed
hanamon <devparkhana@gmail.com>
2021-10-07 17:03:55 +0900

    Delete files in wrong location

--- server/scripts/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
-authbind --deep pm2 start app.js
--- server/scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
-pm2 stop app.js 2> /dev/null || true
-pm2 delete app.js 2> /dev/null || true
