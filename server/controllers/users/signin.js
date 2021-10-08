207794a65896407b8d278d02c5598e98d051ec7e
hanamon <devparkhana@gmail.com>
2021-10-08 23:32:08 +0900

    Just pretty

--- server/controllers/users/signin.js
+++ server/controllers/users/signin.js
@@ -13,22 +13,28 @@ module.exports = async (req, res) => {
     // 이미 로그인 되어있는 경우
     const { accessToken } = req.cookies;
     const accessTokenData = checkAccessToken(accessToken);
-    if (accessTokenData)
+    if (accessTokenData) {
       return res.status(400).json({ message: 'Already logged in!' });
+    }
 
     const { email, password } = req.body;
 
     // 요청이 잘못된 경우
-    if (!email || !password)
+    if (!email || !password) {
       return res.status(400).json({ message: 'Bad Request!' });
+    }
 
     // 등록된 회원이 존재하는지 확인한다.
     const userInfo = await user.findOne({ where: { email } });
-    if (!userInfo) return res.status(403).json({ message: 'Not authorized!' });
+    if (!userInfo) {
+      return res.status(403).json({ message: 'Not authorized!' });
+    }
 
     // 등록된 회원이 존재한다면 비밀번호를 확인한다.
     const match = await bcrypt.compare(password, userInfo.dataValues.password);
-    if (!match) return res.status(403).json({ message: 'Not authorized!' });
+    if (!match) {
+      return res.status(403).json({ message: 'Not authorized!' });
+    }
 
     // 회원의 비밀번호와 역할을 삭제한다.
     delete userInfo.dataValues.password;
--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -11,27 +11,31 @@ module.exports = async (req, res) => {
     // 이미 로그인 되어있는 경우
     const { accessToken } = req.cookies;
     const accessTokenData = checkAccessToken(accessToken);
-    if (accessTokenData)
+    if (accessTokenData) {
       return res.status(400).json({ message: 'Already logged in!' });
+    }
 
     const { login, email, password } = req.body;
 
     // 요청이 잘못된 경우
-    if (!login || !email || !password)
+    if (!login || !email || !password) {
       return res.status(400).json({ message: 'Bad Request!' });
+    }
 
     // 요청받은 정보로 등록된 이미 회원이 존재하는지 확인한다.
     let userInfo = await user.findOne({ where: { login } });
-    if (userInfo)
+    if (userInfo) {
       return res
         .status(409)
         .json({ message: 'This login information cannot be registered' });
+    }
 
     userInfo = await user.findOne({ where: { email } });
-    if (userInfo)
+    if (userInfo) {
       return res
         .status(409)
         .json({ message: 'This email information cannot be registered' });
+    }
 
     // 등록 가능한 정보라면 비밀번호를 암호화하고 새로운 회원을 생성한다.
     const hash = await bcrypt.hash(password, 12);
