ac82847f62231ce39a1681cf0d0432e65f434843
seongjinpark1 <blackdavil015@gmail.com>
2021-10-14 17:09:03 +0900

    feat modify

--- client/src/components/Modal/SignupInput.js
+++ client/src/components/Modal/SignupInput.js
@@ -67,7 +67,6 @@ function SignupInput() {
   };
 
   const checkEmail = (email) => {
-    console.log(email);
     let emailExp =
       /^[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
     if (email === '') {
